36c5f81443c46172826c9077a57353e2fa9002dc
Andrew Tat <tat@andrewtat.com>
2014-10-30 01:12:23 -0700

    Add Day 1

---
+++ Day 1 - Hello World/Day 1 - Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day 1 - Hello World
+//
+//  Created by Andrew Tat on 10/30/14.
+//  Copyright (c) 2014 Andrew Tat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
